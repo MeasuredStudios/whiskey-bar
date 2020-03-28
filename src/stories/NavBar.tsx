2d4584fc8975b4a3c7b9a89a5349083a769c5821
Jovani Pink <jovanirosado@me.com>
2020-03-28 08:33:04 -0400

    init

---
+++ src/stories/NavBar.tsx
@@ -0,0 +1,11 @@
+<NavBar bg="light" expand="lg">
+  <Container fluid>
+    <Navbar.Brand>
+      <img
+        src=""
+        className=""
+        alt=""
+      ></img>
+    </Navbar.Brand>
+  </Container>
+</NavBar>;
